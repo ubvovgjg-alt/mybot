f348bff54e4f76393848cef9c86468ad5b5407e8
ubvovgjg-alt <ubvovgjg@gmail.com>
2026-07-01 15:36:05 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>مرحباً بك في موقع HarbBot!</h1><p>الموقع يعمل الآن بنجاح.</p>');
+});
+
+app.listen(3000, () => {
+    console.log('السيرفر يعمل على المنفذ 3000');
+});
 
